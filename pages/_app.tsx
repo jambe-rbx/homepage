c98b0fe6c8e3a9709343a2303ffda93b51140902
Cam <21029087+csqrl@users.noreply.github.com>
2022-11-15 12:40:11 +0000

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from "next/app"
 
+import { Analytics } from '@vercel/analytics/react';
+
 import "@fontsource/poppins"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+    </>
 }
 
 export default MyApp
